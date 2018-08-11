d50edbc513d199d55ab64105464d9b70610fd187
Roger <rdpender3@gmail.com>
2018-08-11 13:52:53 -0700

    initial commit

---
+++ letter.js
@@ -0,0 +1,4 @@
+// contains Letter constructor 
+// constructor displays underlying character or blank space (underscore) if letter was guessed or not
+// constructor should define: store underlying character, boolean if letter has been guessed, function returning underlying character if its guess, or place underscore if not guessed yet
+// takes character and checks it against underlying character, updates stored boolean value t otrue if guess correctly
---
+++ word.js
@@ -0,0 +1,4 @@
+//contains Word constructor
+// used to create object representing current word the user is trying to guess
+// should contain: array of new letters representing letters from underlying word
+//function that returns a string representing the word. should call on each letter object from letter.js
