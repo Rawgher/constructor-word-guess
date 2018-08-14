c78e1f99ee85fd8d6c4a1b6b580c40c82ee3e77b
Roger <rdpender3@gmail.com>
2018-08-13 22:28:57 -0700

    adding some requirements to files

---
+++ index.js
@@ -0,0 +1,4 @@
+// randomly select words and use Word constructor to store it
+// prompt user for each guess and keeps track of remaining guesses
+
+let Word = require("./word.js")
--- word.js
+++ word.js
@@ -3,3 +3,5 @@
 // should contain: array of new letters representing letters from underlying word
 //function that returns a string representing the word. should call on each letter object from letter.js shows as underscore and concatenate together
 //function takes character as an argument and calls the guess function on each letter from letter.js
+
+let Letter = require("./letter.js")
