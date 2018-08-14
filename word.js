f8a3209708423760009036fef5d71e92a4a29b6b
Roger <rdpender3@gmail.com>
2018-08-13 22:25:38 -0700

    added requirements

--- word.js
+++ word.js
@@ -1,4 +1,5 @@
 //contains Word constructor
 // used to create object representing current word the user is trying to guess
 // should contain: array of new letters representing letters from underlying word
-//function that returns a string representing the word. should call on each letter object from letter.js
+//function that returns a string representing the word. should call on each letter object from letter.js shows as underscore and concatenate together
+//function takes character as an argument and calls the guess function on each letter from letter.js
