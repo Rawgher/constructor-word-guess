6f37f39f56a9184c1894cfb99b886b8e62316b62
Roger <rdpender3@gmail.com>
2018-08-13 23:04:45 -0700

    working on word js

--- word.js
+++ word.js
@@ -4,4 +4,13 @@
 //function that returns a string representing the word. should call on each letter object from letter.js shows as underscore and concatenate together
 //function takes character as an argument and calls the guess function on each letter from letter.js
 
-let Letter = require("./letter.js")
+let Letter = require("./letter.js");
+
+let alpha = "abcdefghijklmnopqrstuvwxyz";
+
+function Word(secretWord) {
+    this.letter = [];
+    for (let i=0; i < secretWord.length; i++) {
+        this.letter.push(new Letter(secretWord[i]));
+    };
+};
